6be792a957cf0d91e3ca22487358700d847b4efd
Fábio Garrote <fgarrote12@gmail.com>
2025-09-07 21:49:40 -0300

    feat: create initial backend config

---
+++ back/app.py
@@ -0,0 +1,11 @@
+from config.database import app, db
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    app.run(
+        host=app.config["HOST"],
+        port=app.config["PORT"],
+        debug=app.config["DEBUG"]
+    )
