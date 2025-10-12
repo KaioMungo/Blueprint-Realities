a745d1e401762644d6a463987833a894c735cabc
Fábio Garrote <fgarrote12@gmail.com>
2025-10-12 16:01:09 -0300

    feat: create simple header

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -3,3 +3,12 @@ const btnLogout = document.getElementById('botao_logout');
 if(localStorage.getItem('user')) {
     btnLogout.setAttribute('style', 'display: block');
 };
+
+function getStarted() {
+    if(localStorage.getItem('user')) {
+        window.location.assign("./HTML/prompt.html")
+        return
+    };
+
+    window.location.assign("./HTML/login.html")
+}
